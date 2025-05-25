db833fc47d031cc877e394c45bb1efaa89167c70
Tomáš Glázr <tomas.glazr@ackee.cz>
2025-05-25 12:59:57 +0200

    Add cluster initialization commands

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# K3s
+curl -sfL https://get.k3s.io | sh -
+
+# ArgoCD
+sudo kubectl create namespace argocd
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+sudo kubectl apply -f argocd-ingress.yml
+sudo kubectl apply -f argocd-disable-internal-tls.yml
+
+# Nginx
+#sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.2/deploy/static/provider/cloud/deploy.yaml
+
+# Metallb
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.9/config/manifests/metallb-native.yaml
+
+# VPN
+kubectl create secret generic windscribe-auth \
+  --namespace=media \
+  --from-file=ovpn=Windscribe-Prague-Vltava.ovpn \
+  --from-file=auth.txt=auth.txt \
+  --from-literal=username="$(head -n 1 auth.txt)" \
+  --from-literal=password="$(tail -n 1 auth.txt)"
