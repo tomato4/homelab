a46f5dfb95e9d5f12f2adc315aeeb4ef812227a1
Tomáš Glázr <tom.gla45@gmail.com>
2025-10-03 00:46:59 +0200

    Authentik custom setup

--- authentik/generate-secret.sh
+++ authentik/generate-secret.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
+mkdir -p secrets
+echo "secret.yaml" > secrets/.gitignore
+
 kubectl create secret generic authentik-secrets \
-  --from-literal=AUTHENTIK_POSTGRESQL__PASSWORD="$(openssl rand -base64 32)" \
-  --from-literal=AUTHENTIK_SECRET_KEY="$(openssl rand -base64 32)" \
+  --from-literal=POSTGRESQL_PASSWORD="$(openssl rand -base64 32)" \
+  --from-literal=SECRET_KEY="$(openssl rand -base64 32)" \
   --namespace authentik \
   --dry-run=client -o yaml > secrets/secret.yaml
 
